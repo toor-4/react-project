15a8110057fee1cdb5c7566a1c1938cd248aacce
frealchar@gmail.com <frealchar@gmail.com>
2024-05-02 19:52:39 -0400

    modified app.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,27 +1,34 @@
-import './App.css';
-
 function App() {
   const subscribeToChannel = () => {
     // Redirect users to subscribe to the YouTube channel
     // window.open('https://youtube.com/@codewithmuh?sub_confirmation=1', '_blank');
-    window.open('https://youtu.be/284cx2fiJm0', '_blank');
+    window.open("https://youtu.be/284cx2fiJm0", "_blank");
+  };
 
-  }
-
-  const checkLinkedInConnection = () => {
+  // const checkLinkedInConnection = () => {
+  function checkLinkedInConnection() {
     // Logic to check if user is connected on LinkedIn
     // var connected = Math.random() < 0.00; // 5% chance of being connected
     // const connected = false && ''
-    const connected = true || 'false' // answer is always true
-    console.log(connected)
-    var resultMessage = connected ? "Yes, you are connected with me on LinkedIn!" : "No, you are not connected with me on LinkedIn. Connect now: <a href='https://www.linkedin.com/in/muhammad-rashid-daha/' target='_blank'>Follow me on LinkedIn</a>";
+    const connected = true || "false"; // answer is always true
+    console.log(connected);
+    var resultMessage = connected
+      ? "Yes, you are connected with me on LinkedIn!"
+      : "No, you are not connected with me on LinkedIn. Connect now: <a href='https://www.linkedin.com/in/muhammad-rashid-daha/' target='_blank'>Follow me on LinkedIn</a>";
     document.getElementById("result").innerHTML = resultMessage;
   }
 
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        textAlign: "center",
+        backgroundColor: "lavender",
+        padding: "30px",
+      }}
+    >
       <h1>Hello World</h1>
-      <button onClick={subscribeToChannel} > Subscribe to my Channel </button>
+      <button onClick={subscribeToChannel}> Subscribe to my Channel </button>
       <p id="result"></p>
       <button onClick={checkLinkedInConnection}>Check Connection</button>
     </div>
